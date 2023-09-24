1d9e7293233cfc652f59c384528bbf6c05cfc108
kamilowski1997 <kamilowski1997@gmail.com>
2023-09-24 16:28:16 +0200

    added prettier and basic navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-function App() {
+import React from 'react';
+import {
+  Navigate,
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route index element={<HomePage />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Route>,
+  ),
+);
+
+const App = () => {
   return (
     <>
-      <div>Hello</div>
+      <RouterProvider router={router} />
     </>
   );
-}
+};
 
 export default App;
